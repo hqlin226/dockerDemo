4495253c670067ce10500a6f045cf7a6696f5051
houqianglin <houqianglin@aerozhonghuan.com>
2017-04-25 10:55:25 +0800

    添加源代码

---
+++ src/main/java/com/example/docker/demoController.java
@@ -0,0 +1,16 @@
+package com.example.docker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by HOUQL on 2017/4/12.
+ */
+@RestController
+public class demoController {
+
+   @RequestMapping("/")
+   public String home(){
+        return "Hello Docker World_"+System.currentTimeMillis()/1000;
+    }
+}
